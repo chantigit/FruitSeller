4cc9ddf53085c5856a422ac22e8436c3df07f7f2
chantigit <chantifacts1@gmail.com>
2021-12-10 06:39:26 +0530

    Log was added

--- src/main/java/com/chanti/api/RestController1.java
+++ src/main/java/com/chanti/api/RestController1.java
@@ -3,6 +3,8 @@ package com.chanti.api;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestController1 {
 
 	private List fruits=Arrays.asList("Mango","Banana","Graphes");
+	private static final Logger LOG=LoggerFactory.getLogger(RestController1.class);
 	
 	@GetMapping("/fruits")
 	public ResponseEntity<?> showFruits(){
+		fruits.forEach((fruit)->{
+			LOG.debug("{}",fruit);
+		});
 		return ResponseEntity.ok(fruits);
 	}
 }
